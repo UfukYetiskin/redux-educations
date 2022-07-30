7273550f4009ce49789e03977da8ebc08b14b8a2
uyetiskin <ufukyetiskin@hotmail.com>
2022-07-30 17:23:31 +0300

    created store page and Provider

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # redux-educations
+
+## How install redux toolkit
+
+```
+npm add @reduxjs/toolkit react-redux
+```
+
+komutu ile redux toolkit' i projemize ekledik. @redux/toolkit ile redux' ı kullanabiliriz. react-redux ile de store' u kullanabiliriz.
+
+### How Create Redux Store file
+
+```
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {},
+})
+```
+
+### How Create Provider Redux in Main Index.js
+
+```
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+//store, bütün state'lerin tutulduğu yer
+import {store} from './redux/store'
+//Proviver ile kendisine gelen bütün dataları sarmaladığı bütün componentlere dağıtmaya yarar.
+import {Provider} from 'react-redux'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+```
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//store, bütün state'lerin tutulduğu yer
+import {store} from './redux/store'
+//Proviver ile kendisine gelen bütün dataları sarmaladığı bütün componentlere dağıtmaya yarar.
+import {Provider} from 'react-redux'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+
 reportWebVitals();
