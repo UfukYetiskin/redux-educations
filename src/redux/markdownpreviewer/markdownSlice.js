6120e639650ac20b02d4ac899e9b4476332af97d
uyetiskin <ufukyetiskin@hotmail.com>
2022-08-20 16:17:10 +0300

    updated clear chars

--- src/redux/markdownpreviewer/markdownSlice.js
+++ src/redux/markdownpreviewer/markdownSlice.js
@@ -18,29 +18,55 @@ const markdownSlice = createSlice({
                 state.element = "strong"
                 state.text = `${toStr}`;
 
-            }else if(value[0] === "#" && value[1] !== "#"){ 
+            }else if(value[0] === "#" && value[1] !== "#"){
+                const toArr = value.split("");
+                toArr.splice(0,1);
+                const toStr = toArr.join("")
                 state.element = "h1"
-                state.text = `<${state.element}>  ${value} </${state.element}>`;
+                state.text = `${toStr}`;
 
             }else if(value[0] === "#" && value[1] === "#" && value[2] !== "#" ){
+                const toArr = value.split("");
+                toArr.splice(0,2);
+                const toStr = toArr.join("")
                 state.element = "h2"
-                state.text = `<${state.element}>  ${value} </${state.element}>`;
+                state.text = `${toStr}`;
 
             }else if(value[0] === "#" && value[1] === "#" && value[2] === "#" && value[3] !== "#"){
+                const toArr = value.split("");
+                toArr.splice(0,3);
+                const toStr = toArr.join("")
                 state.element = "h3"
-                state.text = `<${state.element}>  ${value} </${state.element}>`;
+                state.text = `${toStr}`;
 
             }else if(value[0] === "#" && value[1] === "#" && value[2] === "#" && value[3] === "#" && value[4] !== "#"){
+                const toArr = value.split("");
+                toArr.splice(0,4);
+                const toStr = toArr.join("")
                 state.element = "h4"
-                state.text = `<${state.element}>  ${value} </${state.element}>`;
+                state.text = `${toStr}`;
 
             }else if(value[0] === "#" && value[1] === "#" && value[2] === "#" && value[3] === "#" && value[4] === "#" && value[5] !== "#"){
+                const toArr = value.split("");
+                toArr.splice(0,5);
+                const toStr = toArr.join("")
                 state.element = "h5"
-                state.text = `<${state.element}>  ${value} </${state.element}>`;
+                state.text = `${toStr}`;
             }else if(value[0] === "#" && value[1] === "#"  && value[2] === "#" && value[3] === "#" && value[4] === "#" && value[5] === "#" && value[6] !== "#"){
+                const toArr = value.split("");
+                toArr.splice(0,6);
+                const toStr = toArr.join("")
                 state.element = "h6"
-                state.text = `<${state.element}>${value}</${state.element}>`;
+                state.text = `${toStr}`;
+            }else if(value[0] === "*"  && value[1]  !== "*" && value[value.length-1] === "*" && value[value.length-2] !== "*"){
+                const toArr = value.split("");
+                toArr.splice(0,1)
+                toArr.splice(toArr.length-1, 1)
+                const toStr = toArr.join("");
+                state.element = "i"
+                state.text = `${toStr}`;
             }else{
+                state.element ="p"
                 state.text = value;
             }
 
